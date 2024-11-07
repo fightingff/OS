319ee73876d1c9f063145a3381133b1a6e4cc159
cjlworld <cjlworld@outlook.com>
2024-11-07 17:01:21 +0800

    update

--- src/lab3/arch/riscv/kernel/vm.c
+++ src/lab3/arch/riscv/kernel/vm.c
@@ -2,6 +2,9 @@
 #include "printk.h"
 #include "string.h"
 
+extern char *_stext, *_etext;
+extern char *_srodata, *_erodata;
+
 /* early_pgtbl: 用于 setup_vm 进行 1GiB 的映射 */
 uint64_t early_pgtbl[512] __attribute__((__aligned__(0x1000)));
 
@@ -59,5 +62,52 @@ void setup_vm() {
     early_pgtbl[index] = (PHY_START >> 12) << 10;
     early_pgtbl[index] |= ((1 << 4) - 1);
 
+    LOG(RED "text: %p" CLEAR, _stext);
     printk("...setup_vm done!\n");
+}
+
+/* swapper_pg_dir: kernel pagetable 根目录，在 setup_vm_final 进行映射 */
+uint64_t swapper_pg_dir[512] __attribute__((__aligned__(0x1000)));
+
+void setup_vm_final() {
+    memset(swapper_pg_dir, 0x0, PGSIZE);
+
+    // No OpenSBI mapping required
+    
+    // mapping kernel text X|-|R|V
+    // create_mapping(...);
+
+    // mapping kernel rodata -|-|R|V
+    // create_mapping(...);
+
+    // mapping other memory -|W|R|V
+    // create_mapping(...);
+
+    // set satp with swapper_pg_dir
+
+    // YOUR CODE HERE
+
+    // flush TLB
+    asm volatile("sfence.vma zero, zero");
+
+    // flush icache
+    asm volatile("fence.i");
+    return;
+}
+
+
+/* 创建多级页表映射关系 */
+/* 不要修改该接口的参数和返回值 */
+void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz, uint64_t perm) {
+    /*
+     * pgtbl 为根页表的基地址
+     * va, pa 为需要映射的虚拟地址、物理地址
+     * sz 为映射的大小，单位为字节
+     * perm 为映射的权限（即页表项的低 8 位）
+     * 
+     * 创建多级页表的时候可以使用 kalloc() 来获取一页作为页表目录
+     * 可以使用 V bit 来判断页表项是否存在
+    **/
+
+
 }
